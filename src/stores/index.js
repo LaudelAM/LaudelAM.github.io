54edf92db0978dde9725602504692ee3311c2aa0
Laudel A.M <admin@Admins-MacBook-Pro-2.local>
2020-12-17 14:20:02 +0200

    state management

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,9 @@
+import Vue from "vue";
 import Vuex from "vuex";
 import products from "./products";
 import cart from "./cart";
+
+Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
